ebefe7f9548893b990af2c848c3163fc3e75f870
Cameron <cameronbrady1527@gmail.com>
2025-04-02 22:07:34 -0400

    adding sidebar to blog page

--- app/blog/layout.tsx
+++ app/blog/layout.tsx
@@ -1,10 +1,21 @@
 import '@/app/globals.css';
 import { ReactNode } from 'react'
 
+import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
+import { AppSidebar } from "@/components/app-sidebar";
+
 export default function BlogLayout({ children }: { children: ReactNode }) {
     return (
       <html lang="en">
-        <body>{children}</body>
+        <body>
+          <SidebarProvider>
+            <AppSidebar />
+            <main>
+              <SidebarTrigger />
+              {children}
+            </main>
+          </SidebarProvider>
+        </body>
       </html>
     )
 }
