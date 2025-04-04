4d3305438c123774255544d41ec5e1147732f1d2
Cameron <cameronbrady1527@gmail.com>
2025-04-04 18:17:43 -0400

    a slight refractoring

--- app/blog/ml-concepts/best-practices/page.tsx
+++ app/blog/ml-concepts/best-practices/page.tsx
@@ -1,6 +1,6 @@
 import Link from "next/link";
 
-export default function MLTutorials() {
+export default function MLBestPractices() {
     return (
         <main className="container mx-auto min-h-screen max-w-3xl p-8 flex flex-col gap-4">
             <Link href="/blog" className="hover:underline">
