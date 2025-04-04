aca11f77b1cc579bb037b1b6879053460dc6a4c8
Cameron <cameronbrady1527@gmail.com>
2025-04-03 22:13:52 -0400

    minor styling fix

--- app/blog/layout.tsx
+++ app/blog/layout.tsx
@@ -20,41 +20,37 @@ import {
 
 export default function BlogLayout({ children }: { children: ReactNode }) {
     return (
-      <html lang="en">
-        <body>
-          <SidebarProvider>
-            <AppSidebar />
-            <main>
-            <SidebarInset>
-              <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12">
-                <div className="flex items-center gap-2 px-4">
-                  <SidebarTrigger className="-ml-1" />
-                  <Separator orientation="vertical" className="mr-2 h-4" />
-                  <Breadcrumb>
-                    <BreadcrumbList>
-                      <BreadcrumbItem className="hidden md:block">
-                        <BreadcrumbLink href="#">
-                          ML Concepts
-                        </BreadcrumbLink>
-                      </BreadcrumbItem>
-                      <BreadcrumbSeparator className="hidden md:block" />
-                      <BreadcrumbItem>
-                        <BreadcrumbPage>Algorithms</BreadcrumbPage>
-                      </BreadcrumbItem>
-                    </BreadcrumbList>
-                  </Breadcrumb>
-                </div>
-              </header>
-            </SidebarInset> 
-              <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
-                <div className="rounded-xl bg-muted/50">
-                  {children}
-                </div>
-                <div className="min-h-[100vh] flex-1 rounded-xl bg-muted/50 md:min-h-min" />
-              </div>   
-            </main>
-          </SidebarProvider>
-        </body>
-      </html>
+      <SidebarProvider>
+        <AppSidebar />
+        <main>
+          <SidebarInset>
+            <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12">
+              <div className="flex items-center gap-2 px-4">
+                <SidebarTrigger className="-ml-1" />
+                <Separator orientation="vertical" className="mr-2 h-4" />
+                <Breadcrumb>
+                  <BreadcrumbList>
+                    <BreadcrumbItem className="hidden md:block">
+                      <BreadcrumbLink href="#">
+                        ML Concepts
+                      </BreadcrumbLink>
+                    </BreadcrumbItem>
+                    <BreadcrumbSeparator className="hidden md:block" />
+                    <BreadcrumbItem>
+                      <BreadcrumbPage>Algorithms</BreadcrumbPage>
+                    </BreadcrumbItem>
+                  </BreadcrumbList>
+                </Breadcrumb>
+              </div>
+            </header>
+          </SidebarInset> 
+          <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
+            <div className="rounded-xl bg-muted/50">
+              {children}
+            </div>
+            <div className="min-h-[100vh] flex-1 rounded-xl bg-muted/50 md:min-h-min" />
+          </div>   
+        </main>
+      </SidebarProvider>
     )
 }
