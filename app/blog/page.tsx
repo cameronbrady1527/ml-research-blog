cbcd8ae09588a1c33f83c30b2251b78d84480c25
Cameron <cameronbrady1527@gmail.com>
2025-04-03 22:01:12 -0400

    adding static breadcrumbs to blog pages

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -16,9 +16,9 @@ const options = { next: { revalidate: 30 } };
 
 export default async function BlogHomePage() {
     const posts = await client.fetch<SanityDocument[]>(POSTS_QUERY, {}, options);
-
+    //p-8 flex flex-col gap-4
     return (
-        <main className="container mx-auto min-h-screen max-w-3xl">
+        <main className="container mx-auto min-h-screen max-w-3xl p-8 flex flex-col gap-4">
             <h1 className="text-4xl font-bold mb-8">Posts</h1>
             <ul className="flex flex-col gap-y-4">
                 {posts.map((post) => (
--- components/app-sidebar.tsx
+++ components/app-sidebar.tsx
@@ -1,79 +1,178 @@
-import { 
-    Calendar,
-    Home, 
-    Inbox, 
-    Search, 
-    Settings, 
-    BookOpenText, BellDot, BrainCircuit } from "lucide-react"
+"use client";
 
+import * as React from "react";
+import { 
+  AudioWaveform,
+  BookOpen,
+  Bot,
+  Command,
+  Frame,
+  GalleryVerticalEnd,
+  Map,
+  PieChart,
+  Settings2,
+  BookOpenText, 
+  BellDot, 
+  BrainCircuit, 
+  SquareTerminal,
+  Brain,
+  BotIcon
+} from "lucide-react";
+
+import { NavMain } from "@/components/nav-main";
+import { NavProjects } from "@/components/nav-project";
+import { NavUser } from "@/components/nav-user";
+import { TeamSwitcher } from "@/components/team-switcher";
 import {
     Sidebar,
     SidebarContent,
-    SidebarGroup,
-    SidebarGroupContent,
-    SidebarGroupLabel,
-    SidebarMenu,
-    SidebarMenuButton,
-    SidebarMenuItem,
     SidebarFooter,
     SidebarHeader,
-  } from "@/components/ui/sidebar"
+    SidebarRail
+  } from "@/components/ui/sidebar";
   
-// Menu Items
-const items = [
+// SAMPLE DATA
+const data = {
+  user: {
+    name: "cameronbrady",
+    email: "cameronbrady1527@gmail.com",
+    avatar: ""
+  },
+  teams: [
     {
-        title: "Home",
-        url: "#",
-        icon: Home
+      name: "Acme Inc",
+      logo: GalleryVerticalEnd,
+      plan: "Enterprise",
     },
     {
-        title: "Inbox",
-        url: "#",
-        icon: Inbox
+      name: "Acme Corp",
+      logo: AudioWaveform,
+      plan: "Startup",
     },
     {
-        title: "Calendar",
-        url: "#",
-        icon: Calendar
+      name: "Evil Corp",
+      logo: Command,
+      plan: "Free",
+    },
+  ],
+  navMain: [
+    {
+      title: "ML Concepts",
+      url: "#",
+      icon: SquareTerminal,
+      isActive: true,
+      items: [
+        {
+          title: "Tutorials",
+          url: "#"
+        },
+        {
+          title: "Algorithms",
+          url: "#"
+        },
+        {
+          title: "Math",
+          url: "#"
+        },
+        {
+          title: "Best Practices",
+          url: "#"
+        },
+      ]
     },
     {
-        title: "Search",
-        url: "#",
-        icon: Search
+      title: "Neurological Insights",
+      url: "#",
+      icon: BrainCircuit,
+      isActive: true,
+      items: [
+        {
+          title: "Disorder Explanations",
+          url: "#"
+        },
+        {
+          title: "Current Detection Methods",
+          url: "#"
+        },
+        {
+          title: "Challenges in Diagnosis",
+          url: "#"
+        },
+      ]
     },
     {
-        title: "Settings",
-        url: "#",
-        icon: Settings
+      title: "Project Updates",
+      url: "#",
+      icon: BellDot,
+      isActive: true,
+      items: [
+        {
+          title: "Weekly/Monthly Progress",
+          url: "#"
+        },
+        {
+          title: "Milestones Achieved",
+          url: "#"
+        },
+        {
+          title: "Challenges Overcome",
+          url: "#"
+        },
+      ]
+    },
+    {
+      title: "Learning Journey",
+      url: "#",
+      icon: BookOpenText,
+      isActive: true,
+      items: [
+        {
+          title: "Personal Growth",
+          url: "#"
+        },
+        {
+          title: "Lessons Learned",
+          url: "#"
+        },
+        {
+          title: "Resources Discovered",
+          url: "#"
+        },
+      ]
     }
-];
+  ],
+  projects: [
+    {
+      name: "Parkinsons Detection",
+      url: "#",
+      icon: Brain
+    },
+    {
+      name: "Machine Learning Research Blog",
+      url: "#",
+      icon: SquareTerminal
+    },
+    {
+      name: "Astral AI",
+      url: "#",
+      icon: BotIcon
+    }
+  ]
+}
 
-  export function AppSidebar() {
-    return (
-      <Sidebar>
-        {/* <SidebarHeader /> */}
-        <SidebarContent>
-          <SidebarGroup>
-            <SidebarGroupLabel>Application</SidebarGroupLabel>
-            <SidebarGroupContent>
-              <SidebarMenu>
-                {items.map((item) => (
-                  <SidebarMenuItem key={item.title}>
-                    <SidebarMenuButton asChild>
-                      <a href={item.url}>
-                        <item.icon />
-                        <span>{item.title}</span>
-                      </a>
-                    </SidebarMenuButton>
-                  </SidebarMenuItem>
-                ))}
-              </SidebarMenu>
-            </SidebarGroupContent>
-          </SidebarGroup>
-          <SidebarGroup />
-        </SidebarContent>
-        {/* <SidebarFooter /> */}
-      </Sidebar>
-    )
-  }
-  
+export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
+  return (
+    <Sidebar collapsible="icon" {...props}>
+      {/* <SidebarHeader>
+        <TeamSwitcher teams={data.teams} />
+      </SidebarHeader> */}
+      <SidebarContent>
+        <NavMain items={data.navMain} />
+        <NavProjects projects={data.projects} />
+      </SidebarContent>
+      {/* <SidebarFooter>
+        <NavUser user={data.user} />
+      </SidebarFooter> */}
+    </Sidebar>
+  )
+}
